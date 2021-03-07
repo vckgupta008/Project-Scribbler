8f1ec01bfd34b2d9f337ddb8f14a0950fb3d2200
Vicky Gupta <vicky.gupta@in.imshealth.com>
2021-03-07 19:05:22 +0530

    completing all the functionalities of edit post information page

--- scripts/post.js
+++ scripts/post.js
@@ -1,3 +1,136 @@
-document.getElementById('postComment').value='';
-let userInfo=JSON.parse(sessionStorage.getItem('post-user'));
+let userInfo = JSON.parse(sessionStorage.getItem("post-user"));
 console.log(userInfo);
+var editPost = "",
+  savePost = "",
+  postTitle = "",
+  inputTitle = "",
+  postInformation = "",
+  inputInformation = "";
+printUserInfo(userInfo);
+
+function printUserInfo(userInfo) {
+  let postBody = document.getElementsByClassName("post-body")[0];
+  let postInfo = "";
+  userInfo.map(user => {
+    let userComments = "";
+    if (user.comments.length) {
+        user.comments.forEach(comment => {
+            userComments += `<div class="comments-item">${comment}</div>`;
+        })
+    }
+    postInfo = `
+        <h3 class="post-title">
+            ${user.title}
+        </h3>
+        <input class="input-title" id="inputTitle" value="${
+          user.title
+        }" style="display:none"/>
+        <div class="post-subheading">
+            <div class="post-user">
+                ${user.user}
+            </div>
+            <div class="post-action">
+                <button id="editPost">
+                    Edit <i class="fa fa-edit"></i>
+                </button>
+                <button id="savePost" style="display:none">
+                    Save <i class="fa fa-save"></i>
+                </button>
+            </div>
+        </div>
+        <div class="post-information">
+            ${user.info}
+        </div>
+        <textarea class="input-information" id="inputInformation" style="display:none">${
+          user.info
+        }</textarea>
+        
+        ${
+          user.postLikes == 0
+            ? `<div class="post-like">
+                    <button id="likeButton" onclick="increaseLike()">
+                        <i class="fa fa-thumbs-up"></i> Like
+                    </button>
+                </div>
+                <div class="post-like-text">
+                    Be the first one to like this!
+                </div>`
+            : `<div class="post-like">
+                    <button id="likeButton" onclick="increaseLike()">
+                        <i class="fa fa-thumbs-up"></i> Liked
+                    </button>
+                </div>
+                <div class="post-like-text">
+                    ${user.postLikes} ${
+                user.postLikes == 1 ? `person` : `people`
+              } likes this!
+                </div>
+            `
+        }
+        <textarea id="postComment" placeholder="Leave a comment...">
+
+        </textarea>
+        <div>
+            <button id=postCommentButton onclick="postComments()">
+                Comment
+            </button>
+        </div>
+        <div class="comment-title">
+            All Comments
+        </div>
+        ${
+            user.comments.length
+            ? `
+            <div class="comment-list">
+                ${userComments}
+            </div>
+            `
+            : ""
+        }
+        `;
+  });
+  postBody.innerHTML = postInfo;
+  editPost = document.getElementById("editPost");
+  savePost = document.getElementById("savePost");
+  editPost.addEventListener("click", editPage);
+  savePost.addEventListener("click", saveEditPost);
+  postTitle = document.getElementsByClassName("post-title")[0];
+  inputTitle = document.getElementsByClassName("input-title")[0];
+  postInformation = document.getElementsByClassName("post-information")[0];
+  inputInformation = document.getElementsByClassName("input-information")[0];
+  document.getElementById("postComment").value = "";
+}
+
+let likeButton = document.getElementById("likeButton");
+let postLikeText = document.getElementsByClassName("post-like-text")[0];
+
+function editPage() {
+  editPost.style.display = "none";
+  savePost.style.display = "block";
+  postTitle.style.display = "none";
+  inputTitle.style.display = "block";
+  postInformation.style.display = "none";
+  inputInformation.style.display = "block";
+}
+
+function saveEditPost() {
+  console.log(inputTitle.value);
+  console.log(inputInformation.value);
+  userInfo[0].title = inputTitle.value;
+  userInfo[0].info = inputInformation.value;
+  printUserInfo(userInfo);
+}
+
+function increaseLike() {
+  userInfo[0].postLikes++;
+  printUserInfo(userInfo);
+}
+
+function postComments() {
+  let textAreaComment = document.getElementById("postComment");
+  if (textAreaComment.value) {
+    userInfo[0].comments.push(textAreaComment.value);
+    textAreaComment.value = "";
+    printUserInfo(userInfo);
+  }
+}
--- scripts/postslist.js
+++ scripts/postslist.js
@@ -2,6 +2,8 @@ let postList = [
   {
     id: 1,
     user: "Srishti Gupta",
+    postLikes: 0,
+    comments:[],
     title: "'let' me be a 'const'(ant), not a 'var'(iable)!",
     info:
       "Since JavaScript does not have any type-checking, either of these keywords can be used to declare a variable of any type (datatype) in JavaScript. Though all the three keywords are used for the same purpose, they are different."
@@ -9,6 +11,8 @@ let postList = [
   {
     id: 2,
     user: "Colby Fayock",
+    postLikes: 0,
+    comments:[],
     title: "What is linting and how can it save you time?",
     info:
       "One of the biggest challenges in software development is time. It's something we can't easily get more of, but linting can help us make the most out of the time we have."
@@ -16,6 +20,8 @@ let postList = [
   {
     id: 3,
     user: "Yazeed Bzadough",
+    postLikes: 0,
+    comments:[],
     title: "How to Get More Views on Your Tech Blog",
     info:
       "If you're a developer with a Twitter account, you've already seen everyone and their cat start a blog, YouTube channel, or Patreon. We all want to become stars, or at the very least, a recognizable name in the industry."
@@ -23,6 +29,8 @@ let postList = [
   {
     id: 4,
     user: "Cedd Burge",
+    postLikes: 0,
+    comments:[],
     title: "How to write easily describable code",
     info:
       "When code is not describable using words, most people have to do some mental mapping to turn it in to words. This wastes mental energy, and you run the risk of getting the mapping wrong. Different people will map to different words, which" +
@@ -31,6 +39,8 @@ let postList = [
   {
     id: 5,
     user: "Srishti Gupta",
+    postLikes: 0,
+    comments:[],
     title: "Everything you should know about 'module' & 'require' in Node.js",
     info:
       "Node.js treats each JavaScript file as a separate module. For instance, if you have a file containing some code and this file is named xyz.js, then this file is treated as a module in Node, and you can say that you've created a module" +
